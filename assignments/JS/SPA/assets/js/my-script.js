d4c8978c243be3a25c5d119324781a8ded0cb3d9
Milan-sandeepa <glmsandeepa@gmail.com>
2022-10-08 13:13:10 +0530

    SPA Ui updated

--- assignments/JS/SPA/assets/js/my-script.js
+++ assignments/JS/SPA/assets/js/my-script.js
@@ -1,32 +1,45 @@
 $("#item").css("display", "none");
 $("#customer").css("display", "none");
 $("#placeOrder").css("display", "none");
+$("#orders").css("display", "none");
 
 
-$("#homebtn").click(function () {
+$("#homeBtn").click(function () {
     $("#dashboard").css("display", "block");
     $("#item").css("display", "none");
     $("#customer").css("display", "none");
     $("#placeOrder").css("display", "none");
+    $("#orders").css("display", "none");
 });
 
-$("#itembtn").click(function () {
+$("#itemBtn").click(function () {
     $("#dashboard").css("display", "none");
     $("#item").css("display", "block");
     $("#customer").css("display", "none");
     $("#placeOrder").css("display", "none");
+    $("#orders").css("display", "none");
 });
 
-$("#customerbtn").click(function () {
+$("#customerBtn").click(function () {
     $("#dashboard").css("display", "none");
     $("#item").css("display", "none");
     $("#customer").css("display", "block");
     $("#placeOrder").css("display", "none");
+    $("#orders").css("display", "none");
 });
 
-$("#placeOrderbtn").click(function () {
+$("#placeOrderBtn").click(function () {
     $("#dashboard").css("display", "none");
     $("#item").css("display", "none");
     $("#customer").css("display", "none");
     $("#placeOrder").css("display", "block");
+    $("#orders").css("display", "none");
+});
+
+$("#ordersBtn").click(function () {
+    $("#dashboard").css("display", "none");
+    $("#item").css("display", "none");
+    $("#customer").css("display", "none");
+    $("#placeOrder").css("display", "none");
+    $("#orders").css("display", "block");
 });
