c41cf8ab2251af35c8c4637e9628ba4baa767acb
Milan-sandeepa <glmsandeepa@gmail.com>
2022-10-11 13:04:34 +0530

    Customer model added

---
+++ assignments/JS/SPA/model/Customer.js
@@ -0,0 +1,8 @@
+function customer(customerID,customerName,customerAddress,customerContact){
+    return {
+        id: customerID,
+        name: customerName,
+        address: customerAddress,
+        contact: customerContact
+    }
+}
