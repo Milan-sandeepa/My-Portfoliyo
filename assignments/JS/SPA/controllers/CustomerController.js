b581f9bfb3553a6d07ca6b5c68e2356695e7d40d
Milan-sandeepa <glmsandeepa@gmail.com>
2022-10-11 20:34:26 +0530

    Customer add and delete SPA functions added

---
+++ assignments/JS/SPA/controllers/CustomerController.js
@@ -0,0 +1,193 @@
+//disable tab key of all four text fields using grouping selector in CSS
+$("#txtCustomerID,#txtCustomerName,#txtCustomerAddress,#txtCustomerContact,#txtCustomerUpdateName,#txtCustomerUpdateAddress,#txtCustomerUpdateContact").on('keydown', function (event) {
+    if (event.key == "Tab") {
+        event.preventDefault();
+    }
+});
+
+$("#txtCustomerID,#txtCustomerName,#txtCustomerAddress,#txtCustomerContact").on('keyup', function (event) {
+    checkCusValidity();
+});
+
+$("#txtCustomerID,#txtCustomerName,#txtCustomerAddress,#txtCustomerContact").on('blur', function (event) {
+    checkCusValidity();
+});
+
+$("#txtCustomerID").on('keydown', function (event) {
+    if (event.key == "Enter" && checkCustomer(cusIDRegEx, $("#txtCustomerID"))) {
+        $("#txtCustomerName").focus();
+    } else {
+        focusText($("#txtCustomerID"));
+    }
+});
+
+$("#txtCustomerName").on('keydown', function (event) {
+    if (event.key == "Enter" && checkCustomer(cusNameRegEx, $("#txtCustomerName"))) {
+        focusText($("#txtCustomerAddress"));
+    }
+});
+
+$("#txtCustomerAddress").on('keydown', function (event) {
+    if (event.key == "Enter" && checkCustomer(cusAddressRegEx, $("#txtCustomerAddress"))) {
+        focusText($("#txtCustomerContact"));
+    }
+});
+
+$("#txtCustomerContact").on('keydown', function (event) {
+    if (event.key == "Enter" && checkCustomer(cusContactRegEx, $("#txtCustomerContact"))) {
+        let res = confirm("Do you want to add this Customer.?");
+        if (res) {
+            let customerID = $("#txtCustomerID").val();
+            let customerName = $("#txtCustomerName").val();
+            let customerAddress = $("#txtCustomerAddress").val();
+            let customerContact = $("#txtCustomerContact").val();
+
+            // Customer
+            var customerObject = customer(customerID,customerName,customerAddress,customerContact);
+
+            //add the customers object to the array
+            customers.push(customerObject);
+
+            loadAllCustomers();
+
+            clearCusAllTexts();
+        }
+    }
+});
+
+function loadAllCustomers() {
+    //remove all the table body content before adding data
+    $("#tblCustomer").empty();
+
+
+    // get all customer records from the array
+    for (var customer of customers) {
+        // add those data to the table row
+        var row = `<tr><td class="cusID">${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td><i class="editIcon fa-solid fa-pen-to-square " data-bs-toggle="modal"
+                    data-bs-target="#customerEditModal" style="cursor: pointer"></i> <i class="deleteIcon fa-solid fa-trash-can" style="padding-left: 10px;cursor: pointer" type="button"></i></td></tr>`;
+
+        //then add it to the table body of customer table
+        $("#tblCustomer").append(row);
+    }
+
+    bindRowCustomerClickEvents();
+}
+
+$("#customerAddBtn").click(function () {
+    let res = confirm("Do you want to add this Customer.?");
+    if (res) {
+        let customerID = $("#txtCustomerID").val();
+        let customerName = $("#txtCustomerName").val();
+        let customerAddress = $("#txtCustomerAddress").val();
+        let customerContact = $("#txtCustomerContact").val();
+
+        // Customer
+        var customerObject = customer(customerID,customerName,customerAddress,customerContact);
+
+        //add the customers object to the array
+        customers.push(customerObject);
+
+        loadAllCustomers();
+
+        clearCusAllTexts();
+    }
+});
+
+// Delete Customer and edit icon Set
+
+function bindRowCustomerClickEvents() {
+    $(".editIcon").click(function () {
+
+        // Find the row
+        var row = $(this).closest("tr");
+
+        // Find the text
+        var searchID = row.find(".cusID").text();
+
+        if (searchID!=null){
+
+            let customer =searchCustomer(searchID)
+
+            $('#txtCustomerUpdateID').val(customer.id);
+            $('#txtCustomerUpdateName').val(customer.name);
+            $('#txtCustomerUpdateAddress').val(customer.address);
+            $('#txtCustomerUpdateContact').val(customer.contact);
+
+            console.log(customer);
+
+        }else {
+            alert("No such customer to edit. please try Again");
+        }
+    });
+
+    $(".deleteIcon").click(function () {
+
+        // Find the row
+        var row = $(this).closest("tr");
+
+        // Find the text
+        var deleteID = row.find(".cusID").text();
+
+        let option = confirm("Do you really want to delete customer id :" + deleteID);
+
+        if (option){
+            if (deleteCustomer(deleteID)) {
+                alert("Customer Successfully Deleted..");
+
+            } else {
+                alert("No such customer to delete. please check the id");
+            }
+        }
+    });
+}
+
+function searchCustomer(cusID) {
+    for (let customer of customers) {
+        if (customer.id == cusID) {
+            return customer;
+        }
+    }
+    return null;
+}
+
+function deleteCustomer(customerID) {
+    let customer = searchCustomer(customerID);
+    if (customer != null) {
+        let indexNumber = customers.indexOf(customer);
+        customers.splice(indexNumber, 1);
+        loadAllCustomers();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// update Customer-------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- assignments/JS/SPA/controllers/Validations.js
+++ assignments/JS/SPA/controllers/Validations.js
@@ -1,6 +1,8 @@
 // Item Validation
 
 $("#txtItemID").focus();
+$("#itemAddBtn").attr('disabled',true);
+$("#itemUpdateBtn").attr('disabled',true);
 
 // Item reguler expressions
 const itemIDRegEx = /^(I-)[0-9]{3}$/;
@@ -112,3 +114,133 @@ function clearUpAllTexts() {
 }
 
 // customer validation--------------------------------------------------------------------------------------------
+
+$("#txtCustomerID").focus();
+$("#customerAddBtn").attr('disabled',true);
+$("#customerUpdateBtn").attr('disabled',true);
+
+// Customer regular expressions
+const cusIDRegEx = /^(C-)[0-9]{3}$/;
+const cusNameRegEx = /^[A-z ]{4,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{4,}$/;
+const cusContactRegEx = /^[0-9]{10}$/;
+
+const cusNameUpRegEx = /^[A-z ]{4,20}$/;
+const cusAddressUpRegEx = /^[0-9/A-z. ,]{4,}$/;
+const cusContactUpRegEx = /^[0-9]{10}$/;
+
+let customerValidations = [];
+let customerUpdateValidations = [];
+
+customerValidations.push({reg: cusIDRegEx, field: $('#txtCustomerID'),error:'Customer ID Pattern is Wrong : C-001'});
+customerValidations.push({reg: cusNameRegEx, field: $('#txtCustomerName'),error:'Customer Name Pattern is Wrong : A-z 4-20'});
+customerValidations.push({reg: cusAddressRegEx, field: $('#txtCustomerAddress'),error:'Customer Address Pattern is Wrong : A-z 4-20'});
+customerValidations.push({reg: cusContactRegEx, field: $('#txtCustomerContact'),error:'Customer Contact Pattern is Wrong : 0-9'});
+
+customerUpdateValidations.push({reg: cusNameUpRegEx, field: $('#txtCustomerUpdateName'),error:'Customer Name Pattern is Wrong : A-z 4-20'});
+customerUpdateValidations.push({reg: cusAddressUpRegEx, field: $('#txtCustomerUpdateAddress'),error:'Customer Address Pattern is Wrong : A-z 4-20'});
+customerUpdateValidations.push({reg: cusContactUpRegEx, field: $('#txtCustomerUpdateContact'),error:'Customer Contact Pattern is Wrong : 0-9'});
+
+// Customer Add Validation---------------------------------------------------------------
+
+function checkCusValidity() {
+    let errorCount=0;
+    for (let validation of customerValidations) {
+        if (checkCustomer(validation.reg,validation.field)) {
+            textCusSuccess(validation.field,"");
+        } else {
+            errorCount=errorCount+1;
+            setTextCusError(validation.field,validation.error);
+        }
+    }
+    setCusButtonState(errorCount);
+}
+
+function checkCustomer(regex, txtField) {
+    let inputValue = txtField.val();
+    return regex.test(inputValue) ? true : false;
+}
+
+function textCusSuccess(txtField,error) {
+    if (txtField.val().length <= 0) {
+        defaultText(txtField,"");
+    } else {
+        txtField.css('border', '2px solid green');
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function setTextCusError(txtField,error) {
+    if (txtField.val().length <= 0) {
+        defaultText(txtField,"");
+    } else {
+        txtField.css('border', '2px solid red');
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function setCusButtonState(value){
+    if (value>0){
+        $("#customerAddBtn").attr('disabled',true);
+    }else{
+        $("#customerAddBtn").attr('disabled',false);
+    }
+}
+
+// Customer Update Validation---------------------------------------------------------------
+
+function checkCusUpValidity() {
+    let errorCount=0;
+    for (let validation of customerUpdateValidations) {
+        if (checkCustomerUp(validation.reg,validation.field)) {
+            textCusUpSuccess(validation.field,"");
+        } else {
+            errorCount=errorCount+1;
+            setTextCusUpError(validation.field,validation.error);
+        }
+    }
+    setCusUpdateButtonState(errorCount);
+}
+
+function checkCustomerUp(regex, txtField) {
+    let inputValue = txtField.val();
+    return regex.test(inputValue) ? true : false;
+}
+
+function textCusUpSuccess(txtField,error) {
+    if (txtField.val().length <= 0) {
+        defaultText(txtField,"");
+    } else {
+        txtField.css('border', '2px solid green');
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function setTextCusUpError(txtField,error) {
+    if (txtField.val().length <= 0) {
+        defaultText(txtField,"");
+    } else {
+        txtField.css('border', '2px solid red');
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function setCusUpdateButtonState(value){
+    if (value>0){
+        $("#customerUpdateBtn").attr('disabled',true);
+    }else{
+        $("#customerUpdateBtn").attr('disabled',false);
+    }
+}
+
+function clearCusAllTexts() {
+    $("#txtCustomerID").focus();
+    $("#txtCustomerID,#txtCustomerName,#txtCustomerAddress,#txtCustomerContact").val("");
+    checkCusValidity();
+}
+
+function clearCusUpAllTexts() {
+    $("#txtCustomerUpdateName").focus();
+    $("#txtCustomerUpdateName,#txtCustomerUpdateAddress,#txtCustomerUpdateContact").val("");
+    checkCusUpValidity();
+}
