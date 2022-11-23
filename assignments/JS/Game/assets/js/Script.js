a8fdf1b6bd621c9c8182f82828938fa23ed2ab1b
Milan-sandeepa <glmsandeepa@gmail.com>
2022-11-24 00:16:55 +0530

    Run Animation and moveBackground functions implemented

--- assignments/JS/Game/assets/js/Script.js
+++ assignments/JS/Game/assets/js/Script.js
@@ -1,28 +1,17 @@
-// $(document).ready(function (){
-//     IdleAnimationStart();
-// });
-$(window).on('load',function (){
+$(document).ready(function (){
     IdleAnimationStart();
     PopupUserNameStart();
-})
+});
+// $(window).on('load',function (){
+//
+// })
 
 var charNumber = 0;
+var charRunNumber = 0;
 var IdleAnimationNumber=0;
+var RunAnimationNumber=0;
 var UserName="";
-
-
-function IdleAnimate() {
-
-    if (charNumber == 10) {
-        charNumber = 0;
-    }
-    $("#character").attr("src","assets/img/Idle_"+charNumber+".png");
-    charNumber++;
-}
-
-function IdleAnimationStart(){
-    IdleAnimationNumber=setInterval(IdleAnimate,100);
-}
+var moveBackgroundId=0;
 
 function PopUpUserName(){
     $("#popup").css("display","block");
@@ -47,4 +36,58 @@ $("#playBtn").click(function (){
 
 function PopupNameClose(){
     $("#popup").css("display","none");
+
+    addEventListener("keypress",function (event) {
+        var a=event.code;
+        if (a == "Space" || a == "Enter"){
+            clearInterval(RunAnimationNumber);
+            RunAnimationStart();
+        }
+    })
 }
+// --------------------popUp window End--------------------------------
+function IdleAnimate() {
+
+    if (charNumber == 10) {
+        charNumber = 0;
+    }
+    $("#character").attr("src","assets/img/Idle_"+charNumber+".png");
+    charNumber++;
+}
+
+function IdleAnimationStart(){
+    IdleAnimationNumber=setInterval(IdleAnimate,100);
+}
+// --------------------Idle function End--------------------------------
+
+function RunAnimate() {
+
+    if (charRunNumber == 10) {
+        charRunNumber = 0;
+    }
+    $("#character").attr("src","assets/img/Run_"+charRunNumber+".png");
+    charRunNumber++;
+    CountScore();
+}
+
+function RunAnimationStart(){
+    clearInterval(IdleAnimationNumber);
+    clearInterval(moveBackgroundId);
+    RunAnimationNumber=setInterval(RunAnimate,100);
+    moveBackgroundId=setInterval(moveBackground,100)
+}
+// --------------------Run function End--------------------------------
+var x=0;
+function moveBackground(){
+    x=x-20;
+    $("#display").css("background-position-x",x+"px");
+}
+
+// --------------------Background move function End--------------------------------
+var s=0;
+function CountScore(){
+    $("#scoreCount").text(s);
+    s++;
+}
+
+// --------------------Score function End--------------------------------
